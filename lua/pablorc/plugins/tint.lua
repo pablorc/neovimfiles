5714bf391ebc5fe612219455bef2cb3bdafa8ff0
Pablo Rivera Conde <pabloriveraconde@gmail.com>
2023-10-08 11:17:18 +0200

    Add plugins neoscroll, tint

---
+++ lua/pablorc/plugins/tint.lua
@@ -0,0 +1,9 @@
+return {
+	"levouh/tint.nvim",
+	config = function()
+		require("tint").setup({
+			tint = -55, -- Darken colors, use a positive value to brighten
+			saturation = 0.6
+		})
+	end,
+}
