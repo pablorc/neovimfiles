3fd3696a928e36991db7084141e912221c4a7305
Pablo Rivera Conde <pabloriveraconde@gmail.com>
2023-11-30 10:42:26 +0100

    Remove null-ls code

--- lua/pablorc/plugins/null-ls.lua
+++ lua/pablorc/plugins/null-ls.lua
@@ -34,41 +34,7 @@ return {
 			on_attach = function(client, bufnr)
 				local lsp_format_modifications = require("lsp-format-modifications")
 				lsp_format_modifications.attach(client, bufnr, { format_on_save = true })
-				-- if client.supports_method("textDocument/formatting") then
-				-- 	vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-				-- 	vim.api.nvim_create_autocmd("BufWritePre", {
-				-- 		group = augroup,
-				-- 		buffer = bufnr,
-				-- 		callback = function()
-				-- 			vim.lsp.buf.format({
-				-- 				bufnr = bufnr,
-				-- 				filter = function(client)
-				-- 					return client.name == "null-ls"
-				-- 				end,
-				-- 			})
-				-- 		end,
-				-- 	})
-				-- end
 			end,
 		})
 	end,
-	-- config = function()
-	-- 	require("mason").setup()
-	-- 	require("mason-null-ls").setup({
-	-- 		automatic_setup = true,
-	-- 	})
-	--
-	-- 	local null_ls = require("null-ls")
-	--
-	-- 	null_ls.setup({
-	-- 		sources = {
-	-- 			null_ls.builtins.formatting.prettier,
-	-- 			null_ls.builtins.formatting.stylua,
-	-- 			--null_ls.builtins.completion.spell,
-	-- 		},
-	--
-	-- 		ensure_installed = { "prettier" },
-	-- 		automatic_installation = false,
-	-- 	})
-	-- end,
 }
