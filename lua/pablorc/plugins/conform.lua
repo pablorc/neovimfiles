9240adea8e254e5697e81ae277dc3e7f75d7e84a
Pablo Rivera Conde <pabloriveraconde@gmail.com>
2024-01-27 11:38:24 +0100

    Minor changes

--- lua/pablorc/plugins/conform.lua
+++ lua/pablorc/plugins/conform.lua
@@ -2,39 +2,53 @@ local prettier = { "prettierd", "prettier" }
 
 return {
 	"stevearc/conform.nvim",
-	event = { "BufWritePre" },
-	cmd = { "ConformInfo" },
-	opts = {
-		-- Conform will run multiple formatters sequentially
-		-- Use a sub-list to run only the first available formatter
-		--
-		-- { "stylua", "another" }
-		-- - Runs both
-		--
-		-- { "stylua", { "prettierd", "prettier" } }
-		--  - Runs stylua and one of the following two
-		formatters_by_ft = {
-			lua = { "stylua" },
+	-- event = { "BufWritePre", "BufNewFile" },
+	-- cmd = { "ConformInfo" },
+	config = function()
+		local conform = require("conform")
 
-			css = { prettier },
-			graphql = { prettier },
-			handlebars = { prettier },
-			html = { prettier },
-			javascript = { prettier },
-			javascriptreact = { prettier },
-			json = { prettier },
-			jsonc = { prettier },
-			jsx = { prettier },
-			markdown = { prettier },
-			react = { prettier },
-			scss = { prettier },
-			yaml = { prettier },
-		},
+		conform.setup({
+			-- Conform will run multiple formatters sequentially
+			-- Use a sub-list to run only the first available formatter
+			--
+			-- { "stylua", "another" }
+			-- - Runs both
+			--
+			-- { "stylua", { "prettierd", "prettier" } }
+			--  - Runs stylua and one of the following two
+			formatters_by_ft = {
+				lua = { "stylua" },
+				ruby = { "rubocop" },
 
-		format_on_save = {
-			-- These options will be passed to conform.format()
-			timeout_ms = 500,
-			lsp_fallback = true,
-		},
-	},
+				css = { prettier },
+				graphql = { prettier },
+				handlebars = { prettier },
+				html = { prettier },
+				javascript = { prettier },
+				javascriptreact = { prettier },
+				json = { prettier },
+				jsonc = { prettier },
+				jsx = { prettier },
+				markdown = { prettier },
+				react = { prettier },
+				scss = { prettier },
+				yaml = { prettier },
+			},
+
+			format_on_save = {
+				-- These options will be passed to conform.format()
+				async = false,
+				timeout_ms = 50000,
+				lsp_fallback = true,
+			},
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>cf", function()
+			conform.format({
+				async = false,
+				timeout_ms = 50000,
+				lsp_fallback = true,
+			})
+		end, { desc = "[C]onform [F]ormat" })
+	end,
 }
