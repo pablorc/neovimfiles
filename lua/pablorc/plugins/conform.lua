700218770beeae9a6d60b170e53798051923f0e1
Pablo Rivera Conde <pabloriveraconde@gmail.com>
2024-01-27 08:58:16 +0100

    Fix conform

--- lua/pablorc/plugins/conform.lua
+++ lua/pablorc/plugins/conform.lua
@@ -1,32 +1,39 @@
-return {
-	'stevearc/conform.nvim',
-	opts = {},
-	-- Conform will run multiple formatters sequentially
-	-- Use a sub-list to run only the first available formatter
-	--
-	-- { "stylua", "another" }
-	-- - Runs both
-	--
-	-- { "stylua", { "prettierd", "prettier" } }
-	--  - Runs stylua and one of the following two
-	formatters_by_ft = {
-		lua = { "stylua" },
-		javascript = { { "eslint", { "prettierd", "prettier" } } },
-		javascriptreact = { { "eslint", { "prettierd", "prettier" } } },
-		jsx = { { "eslint", { "prettierd", "prettier" } } },
-		react = { { "eslint", { "prettierd", "prettier" } } },
-		ruby = { "rubocop" },
-	},
+local prettier = { "prettierd", "prettier" }
 
-	format_on_save = {
-		-- These options will be passed to conform.format()
-		timeout_ms = 500,
-		lsp_fallback = true,
-	}
-	-- setup = function()
-	-- 	require("conform").setup({
-	--
-	--
-	-- 	})
-	-- end
+return {
+	"stevearc/conform.nvim",
+	opts = {
+		-- Conform will run multiple formatters sequentially
+		-- Use a sub-list to run only the first available formatter
+		--
+		-- { "stylua", "another" }
+		-- - Runs both
+		--
+		-- { "stylua", prettier }
+		--  - Runs stylua and one of the following two
+		formatters_by_ft = {
+			lua = { "stylua" },
+			-- ruby = { "rubocop" },
+
+			javascript = { prettier },
+			javascriptreact = { prettier },
+			jsx = { prettier },
+			react = { prettier },
+			css = { prettier },
+			scss = { prettier },
+			html = { prettier },
+			json = { prettier },
+			jsonc = { prettier },
+			yaml = { prettier },
+			markdown = { prettier },
+			graphql = { prettier },
+			handlebars = { prettier },
+		},
+
+		format_on_save = {
+			-- These options will be passed to conform.format()
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
+	},
 }
