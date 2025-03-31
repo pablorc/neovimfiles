b928d80d16c18d2646fde3c1bc868bce0646df85
Pablo Rivera Conde <pablo.rivera@marsbased.com>
2025-03-31 11:08:23 +0200

    feat: Add more languages to lsp

--- init.lua
+++ init.lua
@@ -426,12 +426,12 @@ end
 --  the `settings` field of the server config. You must look up that documentation yourself.
 local servers = {
   -- clangd = {},
-  -- gopls = {},
+  gopls = {},
   -- pyright = {},
   -- rust_analyzer = {},
   ts_ls = {},
   eslint = {},
-  -- solargraph = {},
+  solargraph = {},
 
   lua_ls = {
     Lua = {
